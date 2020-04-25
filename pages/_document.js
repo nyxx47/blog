b1f2e6c9c51ffa28704cf8b2ba09f933b6f9e87e
krisna ahroid <krisna@clappingape.com>
2020-04-25 21:03:23 +0700

    change title

--- pages/_document.js
+++ pages/_document.js
@@ -39,6 +39,7 @@ class MyDocument extends Document {
       <html lang="en">
         <Head>
           <meta charSet="utf-8" />
+          <title>Krisna Ahroid | Design & Code</title>
           <meta name="viewport" content="width=device-width, initial-scale=1"/>
           <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
           <link rel='stylesheet' href='/static/css/nprogress.css'/>
